d4166980c9ed999a36fd18478f5ce3bc38ba8420
anthny <anthnyvlkr@gmail.com>
2022-03-21 18:28:55 -0400

    add serilog set up initial startup and di

--- TwitterAPI.Analyzer/Program.cs
+++ TwitterAPI.Analyzer/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                // services.AddHostedService<Worker>();
+            });
+}
